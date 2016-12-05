68b6da72b386cab6aac9dcb1ac4402a368775d0e
Hristo Dobrev <hristo.nankov1@gmail.com>
2016-12-05 21:28:06 +0200

    Removed undefined data from AnswerModel

--- src/Models/AnswerModel.js
+++ src/Models/AnswerModel.js
@@ -3,7 +3,7 @@ import KinveyRequester from '../Services/KinveyRequester';
 class AnswerModel {
 
     getAnswersByArticleId(articleId) {
-        return KinveyRequester.get('appdata', `answers?query={"article_id": "${articleId}"}`, data);
+        return KinveyRequester.get('appdata', `answers?query={"article_id": "${articleId}"}`);
     }
 
     getAllAnswers() {
